f254dc2a3177fe10d762b6f0da01405b1333333c
Shreejai Raj <shreejairaj@gmail.com>
2024-07-05 10:53:09 +1000

    hero section

--- app/page.js
+++ app/page.js
@@ -2,15 +2,20 @@ import Image from "next/image";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Dashboard from "./components/Dashboard";
+import Hero from "./components/Home/Hero";
+import Search from "./components/Home/Search";
+import GameList from "./components/Home/GameList";
 
 export default function Home() {
   return (
     <>
       <Header/>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <Dashboard/>
+      <div className="px-5 sm:px-7 md:px-10 mt-9"> 
+        <Hero/>
+        <Search/>
+        <GameList/>
+        <Dashboard/>
+      </div>
       <Footer/>
     </>
   );
