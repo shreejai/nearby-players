1b680a8a26b17da774011878e5712dd361e88b6f
Shreejai Raj <shreejairaj@gmail.com>
2024-07-05 14:22:02 +1000

    firebase setup and data fetch

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+'use client';
 import Image from "next/image";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -5,8 +6,27 @@ import Dashboard from "./components/Dashboard";
 import Hero from "./components/Home/Hero";
 import Search from "./components/Home/Search";
 import GameList from "./components/Home/GameList";
+import Posts from "./components/Home/Posts";
+import app from '../shared/FirebaseConfig';
+import { getFirestore, collection, getDocs, doc } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import { useEffect } from "react";
 
 export default function Home() {
+  // Initialize Firebase
+  const db = getFirestore(app);
+
+  useEffect(()=>{
+    getPost();
+  },[])
+
+  const getPost = async () => {
+    const querySnapshot = await getDocs(collection(db, "posts"));
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+  }
   return (
     <>
       <Header/>
@@ -14,6 +34,7 @@ export default function Home() {
         <Hero/>
         <Search/>
         <GameList/>
+        <Posts/>
         <Dashboard/>
       </div>
       <Footer/>
