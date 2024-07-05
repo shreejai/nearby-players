c35ddc310d66a5784319b071808b0f62b239aaae
Shreejai Raj <shreejairaj@gmail.com>
2024-07-05 11:49:00 +1000

    games list

--- app/components/Home/GameList.jsx
+++ app/components/Home/GameList.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react';
+import GamesList from '../../../shared/Data'
 
 const GameList = () => {
+  const [games, setGames] = useState([]);
+  useEffect(()=>{
+    setGames(GamesList) 
+ },[])
+  console.log("games:",games);
   return (
-    <div>GameList</div>
+    <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 mt-4'>
+      {games.map((item)=>(
+        <div key={item.id} className='flex flex-col items-center text-center cursor-pointer py-2'>
+          <img src={item.image} width={45} height={45} className='hover:animate-bounce transition-all duration-150'/> 
+          <h2 className='text-[14px]'>{item.name}</h2>
+        </div>
+      ))}
+    </div>
   )
 }
 
---
+++ shared/Data.js
@@ -0,0 +1,42 @@
+const GamesList=[
+  {
+      id:1,
+      name:'Cricket',
+      image:'./CricketBall.png'
+  },
+  {
+      id:2,
+      name:'Tennis',
+      image:'./Tennis.png'
+  },
+  {
+      id:3,
+      name:'Ping Pong',
+      image:'./PingPong.png'
+  },
+  {
+      id:4,
+      name:'Soccer',
+      image:'./SoccerBall.png'
+  },
+  {
+      id:5,
+      name:'Badminton',
+      image:'./Badminton.png'
+  },
+  {
+      id:6,
+      name:'Trekking',
+      image:'./Trekking.png'
+  },
+  {
+      id:7,
+      name:'Other Games',
+      image:'./Puzzle.png'
+  },
+
+
+]
+
+export default GamesList
+ 
